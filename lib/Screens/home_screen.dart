16e63e9d7f0a77ebb50a5dcb74fa3ca59d0d7e3f
Sheetal-Rawat87 <rajputsheetal430@gmail.com>
2022-04-29 11:56:06 +0530

    no message

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,13 +1,62 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int _currentIndex=0;
+
+  final tabs=[
+    Center(child: Text('Home')),
+    Center(child: Text('No Booking Available')),
+    Center(child: Text('Help Center')),
+    Center(child: Text('Profile')),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:Text("GeeksForGeeks")),
-      body: Center(
-          child:Text("Home page",textScaleFactor: 2,)
+      appBar: AppBar(
+        title: Text('DoorStep Solution'),
       ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+      type: BottomNavigationBarType.fixed,
+      items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+      icon: Icon(Icons.home),
+      label: 'Home',
+        backgroundColor: Colors.indigo
+
+    ),
+    BottomNavigationBarItem(
+    icon: Icon(Icons.library_books_outlined),
+    label: 'Bookings',
+        backgroundColor: Colors.indigo
+    ),
+    BottomNavigationBarItem(
+    icon: Icon(Icons.help_outline_rounded),
+    label: 'Help Center',
+        backgroundColor: Colors.indigo
+    ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+            backgroundColor: Colors.indigo
+        ),
+    ],
+        onTap: (index){
+        setState(() {
+          _currentIndex = index;
+        });
+        },
+
+    ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,105 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:onestop_solution/Screens/home_screen.dart';
 
 
-void main() {
-  runApp(MaterialApp(
-    //title: "Input Widget",
-    home: MyHomePage(),
-    debugShowCheckedModeBanner: false,
-  ));
-
+void main(){
+  runApp(MyApp());
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-//final TextCtrl = new TextEditingController();
-  String inputStr =" ";
-  bool enabled = false;
-  bool expanded = false;
-  bool checked = false;
-  late String radiovalue;
-
-  int _value = 1;
-
-
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DoorStep Solution'),
+    return MaterialApp(
+      title: 'DoorStep Solution',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo
       ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextField(
-              onChanged: (String textInput) {
-                setState(() {
-                  inputStr = textInput;
-                });
-              },
-              // controller: TextCtrl,
-            ),
-            Text(inputStr),
-            Switch(
-              onChanged: (bool val){
-                setState(() {
-                  enabled = val;
-                  print(val);
-                });
-              },
-              activeColor: Colors.green,
-              activeTrackColor: Colors.greenAccent[400],
-              value: enabled,
-            ),
-            ExpansionPanelList(
-              expansionCallback: (i,bool val){
-                setState((){
-                  expanded = !val;
-                });
-              },
-              children: [
-                ExpansionPanel(
-                  body: Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: Text('Hello'),
-                  ),
-                  headerBuilder:  (BuildContext context, bool val){
-                    return Center(
-                      child:  Text('Tap on me',style: TextStyle(fontSize: 18.0),),
-                    );
-                  },
-                  isExpanded: expanded,
-                ),
-
-
-              ],
-            ),
-            Radio(value: 1, groupValue: _value,
-                onChanged:(String){
-                  radiovalue;
-                }
-            ),
-            Radio(value: 2, groupValue: _value,
-                onChanged:(String){
-                  radiovalue;
-
-                }
-            ),
-            Radio(value: 3, groupValue: _value,
-                onChanged:(String){
-                  radiovalue;
-
-                }
-            )
-          ],
-        ),
-      ),
+      home: HomePage(),
     );
   }
+
 }
